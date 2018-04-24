62e05cd50d5f7b34ef5ff0de0b7270311a9713be
Ivan Yelizariev <yelizariev@it-projects.info>
2018-04-24 23:42:07 +0500

    [FIX] sync with latest version of website_multi_theme

--- website_multi_company/models/website_theme.py
+++ website_multi_company/models/website_theme.py
@@ -26,37 +26,63 @@ class WebsiteTheme(models.Model):
     def _convert_assets(self):
         """Generate assets for converted themes"""
         Asset = self.env["website.theme.asset"]
-        for one in self.filtered("converted_theme_addon"):
-            # Get all views owned by the converted theme addon
-            refs = self.env["ir.model.data"].search([
-                ("module", "in", [one.converted_theme_addon] + one.dependency_ids.mapped('name')),
-                ("model", "=", "ir.ui.view"),
-            ])
-            existing = frozenset(one.mapped("asset_ids.name"))
-            expected = frozenset(refs.mapped("complete_name"))
+
+        common_refs = self.env["ir.model.data"]
+
+        # add views with customize_show menu, so we can activate them per
+        # website independently
+        common_refs |= self.env['ir.ui.view']\
+                           .with_context(active_test=False)\
+                           .search([
+                               ('website_id', '=', False),
+                               ('customize_show', '=', True),
+                           ]).mapped('model_data_id')
+        _logger.debug('common_refs: %s', common_refs.mapped('complete_name'))
+
+        for one in self:
+            refs = self.env["ir.model.data"]
+
+            if one.converted_theme_addon:
+                # Get all views owned by the converted theme addon
+                refs |= self.env["ir.model.data"].search([
+                    ("module", "in", [one.converted_theme_addon] + one.dependency_ids.mapped('name')),
+                    ("model", "=", "ir.ui.view"),
+                ])
+
+            if refs or one.asset_ids:
+                # add common_refs only for installed themes
+                refs |= common_refs
+
+            # Skip views without inherit_id, because those have new
+            # template definition only and after appliying multi-theme
+            # for several websites it will be copied and then it leads
+            # to error on compilation when any template with <t
+            # t-call="..."/> -- expected singleton
+            views = self.env["ir.ui.view"].with_context(active_test=False) \
+                .search([
+                    ("id", "in", refs.mapped("res_id")),
+                    ("type", "=", "qweb"),
+                    ("inherit_id", "!=", False),
+                ])
+            existing = frozenset(
+                one
+                .mapped("asset_ids")
+                .filtered("auto")
+                .mapped("name")
+            )
+            expected = frozenset(views.mapped("xml_id"))
+
             dangling = tuple(existing - expected)
             # Create a new asset for each theme view
             for ref in expected - existing:
-                view = self.env.ref(ref, raise_if_not_found=False)
-                if view and (view.type != 'qweb' or not view.inherit_id):
-                    # Skip non-qweb (backend) views
-
-                    # Skip views without inherit_id, because those have new
-                    # template definition only and after appliying multi-theme
-                    # for several websites it will be copied and then it leads
-                    # to error on compilation when any template with <t
-                    # t-call="..."/> -- expected singleton
-                    continue
-
                 _logger.debug("Creating asset %s for theme %s", ref, one.name)
-
                 priority = 10
                 if view.model_data_id.module == one.converted_theme_addon:
                     # make less priority to apply views after all deps
                     priority = 100
-
                 one.asset_ids |= Asset.new({
                     "name": ref,
+                    "auto": True,
                     'priority': priority,
                 })
             # Delete all dangling assets
